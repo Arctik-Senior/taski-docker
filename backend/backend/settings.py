9b9c3caf3ce22284f870b1a7d6f30a81410f9454
Александр Галушкин <stas.basov@whatever.com>
2025-07-06 13:47:18 +0300

    g

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -12,7 +12,7 @@ DEBUG = True
 ALLOWED_HOSTS = ['89.169.162.86', '127.0.0.1', 'localhost', 'takski.zapto.org']
 
 
-# Application definitionZdddddd
+# Application definitionZdddddd4123
 
 INSTALLED_APPS = [
     'api.apps.ApiConfig',
